237eccadaa24757b8e55ce21103e4284ad27ef10
Takuya Murakami <tmurakam@tmurakam.org>
2024-10-20 16:22:23 +0900

    initial import

---
+++ DuoScore/DuoScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuoScoreApp.swift
+//  DuoScore
+//
+//  Created by 村上 卓弥 on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct DuoScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DuoScoreTests/DuoScoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  DuoScoreTests.swift
+//  DuoScoreTests
+//
+//  Created by 村上 卓弥 on 2024/10/20.
+//
+
+import Testing
+@testable import DuoScore
+
+struct DuoScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
